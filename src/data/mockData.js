57412306100a7a7ae7a8416dde79255a7141a6b4
AymanPoly <88197801+AymanPoly@users.noreply.github.com>
2023-06-16 03:15:34 +0100

    Json
    change the static array list to a dynamic json files easy to be connected with data base

---
+++ src/data/mockData.js
@@ -0,0 +1,24 @@
+import { tokens } from "../theme";
+import data from './mockDataTeam.json';
+import Contacts from './Contancts.json';
+import inovoices from './mockDataInvoices.json';
+import mockTrans from './mockTransactions.json';
+import MockBar from './mockBarData.json';
+import Pie from './mockPieData.json';
+import mockLine from './mockLineData.json';
+import Geo from './mockGeographyData.json';
+export const mockDataTeam =  JSON.parse(data);
+
+export const mockDataContacts = JSON.parse(Contacts);
+
+export const mockTransactions = JSON.parse(mockTrans);
+
+export const mockDataInvoices =JSON.parse(inovoices);
+
+export const mockBarData = JSON.parse(MockBar);
+
+export const mockPieData = JSON.parse(Pie);
+
+export const mockLineData = JSON.parse(mockLine);
+
+export const mockGeographyData = JSON.parse(Geo);
